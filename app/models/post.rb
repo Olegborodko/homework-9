fe3564b4ddcd28a67e771ee3a3916763041ddc2c
Olegborodko <borodkoleg@gmail.com>
2017-02-07 01:18:51 +0300

    correct space fix2

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
- belongs_to :user
+  belongs_to :user
 
- validates :title,
-           presence: {message: 'is require'},
-           length: { in: 3..300,  message: 'max length 300 symbol, min 3'}
- validates :text,
-           presence: {message: 'is require'},
-           length: { in: 5..500,  message: 'max length 500 symbol, min 5'}
+  validates :title,
+            presence: {message: 'is require'},
+            length: { in: 3..300,  message: 'max length 300 symbol, min 3'}
+  validates :text,
+            presence: {message: 'is require'},
+            length: { in: 5..500,  message: 'max length 500 symbol, min 5'}
 
- validates :user,
-           presence: {message: 'is require'}
+  validates :user,
+            presence: {message: 'is require'}
 end
